de02849008d3649632441da9d59c868740ebf535
shagu <ericmauser@googlemail.com>
2019-10-30 21:04:23 +0100

    webls: add custom config

---
+++ .webls-config.lua
@@ -0,0 +1,30 @@
+return {
+  -- generic website options
+  title       = "ericmauser",
+  description = "just some random notes",
+  website     = "https://www.ericmauser.de",
+
+  -- add "index.html" to the end of each link
+  pagesuffix  = true,
+
+  -- folder that shall be scanned for content
+  scanpath    = "content",
+
+  -- the output directory that is later used as webroot
+  www         = "www",
+
+  -- modules that should be used for parsing
+  modules     = { "markdown", "git", "gallery", "download", "footer" },
+
+  -- define default colors
+  colors      = {
+    ["accent"]      = "#3a5",
+    ["border"]      = "#eee",
+    ["bg-page"]     = "#fafafa",
+    ["bg-content"]  = "#fff",
+    ["bg-sidebar"]  = "#fff",
+    ["fg-page"]     = "#000",
+    ["fg-sidebar"]  = "#222",
+  },
+}
+
